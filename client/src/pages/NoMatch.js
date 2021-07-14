b8ff25b34e9a5840ce90ca40eac9dbd056e91a96
Kevin <millhousekevin@gmail.com>
2021-07-13 21:08:56 -0400

    added bootstrap and nomatch page

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function NoMatch(){
+  return (
+    <div>
+
+    <h1>404 Page Not Found</h1>
+    <h1>
+    <span>
+    🙄
+    </span>
+    </h1>
+    </div>
+  )
+}
