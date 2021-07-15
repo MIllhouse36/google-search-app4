7de2b7498011e74be30be9df900f543ae9287431
Kevin <millhousekevin@gmail.com>
2021-07-15 06:16:59 -0400

    adding book model

---
+++ models/book.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, require: true},
+  subtitle: { type: String },
+  authors: { type: [String], require: true},
+
+})
