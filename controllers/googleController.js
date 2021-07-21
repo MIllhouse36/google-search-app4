80b6a19d4e5280cbc2e1827eb4952f5c2e5e5afc
Kevin <millhousekevin@gmail.com>
2021-07-21 12:48:26 -0400

    setting up controller for google api

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -2,13 +2,31 @@ const axios = require("axios");
 const db = require("../models");
 
 module.exports ={
-  findAll: (req, res)=>{
+  findAll: function(req, res) {
     const { query: params } = req;
     axios
-    .get("", { params 
+    .get("https://www.googleapis.com/books/v1/volumes", {
+      params
     })
     .then(results =>
-      results
+      results.data.items.filter(
+        result =>
+        result.volumeInfo.title&&
+        result.volumeInfo.infoLink &&
+        result.volumeInfo.authors &&
+        result.volumeInfo.description &&
+        result.volumeInfo.imageLinks &&
+        result.volumeInfo.imageLinks.thumbnail
       )
+    )
+    .then(apiBooks =>
+      db.Book.find().then(dbBooks =>
+        apiBooks.filter(apiBook =>
+          dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id )
+        )
+      )
+    )
+    .then(books => res.json(books))
+    .catch(err => res.status(422).json(err));
   }
-}
+};
