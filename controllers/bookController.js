b3d843572491b8835c743ab47e89962f5ce0d0e0
Kevin <millhousekevin@gmail.com>
2021-07-24 20:16:30 -0400

    added update controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -14,7 +14,13 @@ module.exports = {
     db.Book.create(req.body)
     .then(dbBook => res.json(dbBook))
     .catch(err => res.status(422).json(err));
-  }
+  },
+  update: function(req, res) {
+    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+    .then(dbBook => res.json(dbBook))
+    .catch(err => res.status(422).json(errr));
+  }, 
+  // remove: function
   
 
 };
