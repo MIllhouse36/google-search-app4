57e74c37815257df14ff935318030991d640352e
Kevin <millhousekevin@gmail.com>
2021-07-28 21:18:11 -0400

    coverting no match page to react-bootstrap

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Container, Row, Col, Jumbotron} from "react-bootstrap";
-export default function NoMatch(){
+import { Container, Row, Col, Jumbotron } from "react-bootstrap";
+export default function NoMatch() {
   return (
-    <div>
-
-    <h1>404 Page Not Found</h1>
-    <h1>
-    <span>
-    🙄
-    </span>
-    </h1>
-    </div>
-  )
+    <Container>
+      <Row>
+        <Col md={12}>
+          <Jumbotron>
+            <h1>404 Page Not Found</h1>
+            <h1>
+              <span>🙄</span>
+            </h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
