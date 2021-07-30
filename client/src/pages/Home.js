5a2f5d36fda1e480abe4e300c2305a679e8e8f10
Kevin <millhousekevin@gmail.com>
2021-07-30 02:42:28 -0400

    add bootstrap to home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {Jumbotron} from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Col } from "react-bootstrap"
