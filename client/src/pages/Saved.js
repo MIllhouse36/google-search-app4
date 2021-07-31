d6422192fb4ced04085cf1baa8a7f4824f9b7c2c
Kevin <millhousekevin@gmail.com>
2021-07-31 10:58:00 -0400

    adding react-bootstrap component to project

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
