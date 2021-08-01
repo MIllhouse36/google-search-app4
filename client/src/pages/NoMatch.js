ec2b32e66e2ff27e27798599e99307cc9e21343a
Kevin <millhousekevin@gmail.com>
2021-08-01 15:14:08 -0400

    starting nomatch component and adding api services

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -6,9 +6,11 @@ export default function NoMatch() {
       <Row>
         <Col md={12}>
           <Jumbotron>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span>🙄</span>
+            <h1 className="text-center">404 Page Not Found</h1>
+            <h1 className="text-center">
+              <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                🙄
+              </span>
             </h1>
           </Jumbotron>
         </Col>
