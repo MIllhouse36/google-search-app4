2ea11417b49ab8b128f4bcd5c04514f8c4fa9fe5
Kevin <millhousekevin@gmail.com>
2021-08-02 17:14:04 -0400

    adding to utilities

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,8 @@ export default{
 
   getBooks: function(q){
     return axios.get("/api/google", { params: { q: "title:" + q }})
+  },
+  getSavedBooks: function(){
+    return axios.get("api/books");
   }
 }
