03927722ca724d09d806c7e68e4539717f476881
Kevin <millhousekevin@gmail.com>
2021-08-06 06:45:56 -0400

    setting up saved component

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
      <Nav/>
      <Switch>
        <Route exact path="/" component={Home}/>
-       <Route/>
+       <Route exact path= "/saved" component={Saved}/>
        <Route/>
      </Switch>
     </div>
