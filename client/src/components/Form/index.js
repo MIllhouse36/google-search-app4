8927de6427e1cbe47fde9efe4331674a8e0e6f1b
Kevin <millhousekevin@gmail.com>
2021-08-15 18:26:41 -0400

    building form component

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Form } from "react-bootstrap";
+import { FormGroup } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
 
-export default function Form(){
+export default function Form({q, handleInputChange, handleFormSubmit}){
   return(
-    <form>
-      <div>
-        
-      </div>
-    </form>
+    <Form>
+      <FormGroup>
+      <label htmlFor="Query">
+        <strong>Book</strong>
+      </label>
+      <FormControl 
+      id="Title"
+      type="text"
+      value={q}
+      placeholder="Ready Player One"
+      name="q"
+      onChange={handleInputChange}
+      required      
+      />
+      </FormGroup>
+      
+    </Form>
   )
 }
