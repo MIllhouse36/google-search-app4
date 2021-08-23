011b577cbeb576278b7cece3fb258b83b02d0389
Kevin <millhousekevin@gmail.com>
2021-08-22 21:47:36 -0400

    adding to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,27 @@
 import './style.css';
 import { Link } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
+import { Button } from "react-bootstrap"
 
 
 function Nav() {
+    
+  const [open, setOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  
+  let updateWidth = () => {
+    const newState = { width: window.innerWidth};
+
+    if(open && newState.width > 991){
+      newState.open = false;
+    }
+    setWidth(newState);
+  }
+
+  let toggleNav = () => {
+    setOpen(!open)
+  }
+
   return (
     <Navbar expand="lg" variant="light" bg="light" className="mb-2" >
      <Navbar.Brand as={Link}>
