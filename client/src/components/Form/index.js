059540051e3cb4b7aca822a7482dc51c987d6ea2
Kevin <millhousekevin@gmail.com>
2021-09-03 20:22:56 -0400

    formatting form comp

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,32 +1,34 @@
 import React from "react";
-import  Form  from "react-bootstrap/Form";
-import  FormGroup  from "react-bootstrap/FormGroup";
-import  FormControl  from "react-bootstrap/FormControl";
-import Button  from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
-export default function Formmy({q, handleInputChange, handleFormSubmit}){
-  return(
+export default function Formmy({ q, handleInputChange, handleFormSubmit }) {
+  return (
     <Form>
       <FormGroup>
-      <label htmlFor="Query">
-        <strong>Book</strong>
-      </label>
-      <FormControl 
-      id="Title"
-      type="text"
-      value={q}
-      placeholder="Ready Player One"
-      name="q"
-      onChange={handleInputChange}
-      required      
-      />
+        <label htmlFor="Query">
+          <strong>Book</strong>
+        </label>
+        <FormControl
+          id="Title"
+          type="text"
+          value={q}
+          placeholder="Ready Player One"
+          name="q"
+          onChange={handleInputChange}
+          required
+        />
       </FormGroup>
-        <Button onClick={handleFormSubmit}
+      <Button
+        onClick={handleFormSubmit}
         type="submit"
         className="float-end "
-        variant="danger">
-          search
-        </Button>
+        variant="danger"
+      >
+        search
+      </Button>
     </Form>
-  )
+  );
 }
