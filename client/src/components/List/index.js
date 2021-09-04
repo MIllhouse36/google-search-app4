198903e460f4218a622a7e5b8f55eca6c5ce92a1
Kevin <millhousekevin@gmail.com>
2021-09-04 07:03:16 -0400

    formatting

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import { ListGroup } from "react-bootstrap";
 import { ListGroupItem } from "react-bootstrap";
 
-export const List = ({ children }) => (
-  <ListGroup as ="ul" >
-    {children}
-  </ListGroup>
-)
+export const List = ({ children }) => <ListGroup as="ul">{children}</ListGroup>;
 
-export function ListItem({children}){
-  return <ListGroupItem>{children}</ListGroupItem>
+export function ListItem({ children }) {
+  return <ListGroupItem>{children}</ListGroupItem>;
 }
