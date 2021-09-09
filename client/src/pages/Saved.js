a7f3bfe32647db393f762e149a8f1f6ce3e1e37b
Kevin <millhousekevin@gmail.com>
2021-09-09 15:29:46 -0400

    useEffect and Saved book api set up

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Jumbotron  from "react-bootstrap/Jumbotron";
 import Card  from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import API from "../utils/API";
+import { set } from "mongoose";
 
 export default function Saved (){
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    API.getSavedBooks();
+    })
+    
+  const getSavedBooks = ()=>{
+    API.getSavedBooks()
+    .then(res=> 
+      setBooks(res.data))
+      .catch(err=>console.log(err))
+  }
   return(
     <div>
     </div>
