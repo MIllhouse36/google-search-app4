0ae8e07f35b4be962737baebfca6cafd9b08a546
Kevin <millhousekevin@gmail.com>
2021-09-15 10:45:51 -0400

    saved component doing its thing

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import Jumbotron  from "react-bootstrap/Jumbotron";
-import Card  from "react-bootstrap/Card";
+import Card from "../components/Card"
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
