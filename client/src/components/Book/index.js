e56bb1ca24e1f8e2189405e35b93db11eae8d63e
Kevin <millhousekevin@gmail.com>
2021-09-16 16:46:10 -0400

    sizing and styling

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -49,11 +49,11 @@ function Book({ title, subtitle, authors, link, description, image, Button }) {
   return (
     <ListItem>
       <Row className="flex-wrap-reverse">
-        <Col size="md-8">
+        <Col className="col-md-8">
           <h3 className="font-italic">{title}</h3>
           {subtitle && <h5 className="font-italic">{subtitle}</h5>}
         </Col>
-        <Col size="md-4">
+        <Col className="col-md-4">
           <div className="btn-container">
             <a className="btn btn-light" target="_blank" rel="noopener noreferrer" href={link}>
               View
@@ -63,15 +63,15 @@ function Book({ title, subtitle, authors, link, description, image, Button }) {
         </Col>
       </Row>
       <Row>
-        <Col size="md-6">
+        <Col className="col-md-6">
           <p className="font-italic small">Written by {authors}</p>
         </Col>
       </Row>
       <Row>
-        <Col size="12 sm-4 md-2">
+        <Col className="col-12 col-sm-4 col-md-2">
           <img className="img-thumbnail img-fluid w-100" src={image} alt={title} />
         </Col>
-        <Col size="12 sm-8 md-10">
+        <Col className="col-12 col-sm-8 col-md-10">
           <p>{description}</p>
         </Col>
       </Row>
